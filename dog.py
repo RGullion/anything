1176a7a1945496567c8cd62bb6a5be0b00da5669
Renn <renngullion1@gmail.com>
2017-04-13 16:28:23 -0500

    first commit

---
+++ dog.py
@@ -0,0 +1,15 @@
+import random
+import time
+import sys
+_menu_ = True
+def cats():
+    print("Hello")
+def menu():
+    print("This is print")
+    menuchoice = input("Dogs or cats")
+    if menuchoice == 'yes':
+        cats()
+    elif menuchoice == 'no':
+        pass
+def cats():
+    print("Hello")
---
+++ test.py
@@ -0,0 +1,14 @@
+coppercoins = 0
+silvercoins = 0
+goldcoins = 0
+monsterhealth = 25
+playerhealth = 10
+_sword_ = False
+_shield_ = False
+_monster_ = False
+def Menu():
+    addcoin = input('Add Coin?')
+    if addcoin == 'yes':
+        coppercoins + 1
+    elif addcoin == 'no':
+        pass
